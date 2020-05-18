ffe3f1424455793b7e01f89b494a9ffc5859129e
Darren Ethier <darren@roughsmootheng.in>
2020-05-18 12:55:29 -0400

    Add numerous improvements to get ready for "release". (#19)
    * Treat colon as sentence stop for title extraction
    
    * handle dashes as list items in todo body
    
    * Tweak issue templates adding space
    
    * standardize on template addition of username
    
    * improve templates (add a rocket emojii and make it clearer it’s a bot)
    
    * add remaining tests and fix things exposed by tests
    
    * Add linting and various build configuration tools
    
    - this also fixes all the linting errors after adding the linting.
    
    * remove todo that has been addressed.

--- tests/helpers.js
+++ tests/helpers.js
@@ -1,82 +1,86 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require( 'fs' );
+const path = require( 'path' );
 
-const loadDiff = (exports.loadDiff = (filename) => {
-  return Promise.resolve({
-    data: fs.readFileSync(
-      path.join(__dirname, "fixtures", "diffs", filename + ".txt"),
-      "utf8"
-    ),
-    headers: { "content-length": 1 },
-  });
-});
+const loadDiff = ( exports.loadDiff = ( filename ) => {
+	return Promise.resolve( {
+		data: fs.readFileSync(
+			path.join( __dirname, 'fixtures', 'diffs', filename + '.txt' ),
+			'utf8'
+		),
+		headers: { 'content-length': 1 },
+	} );
+} );
 
-exports.gimmeContext = (eventName, action) => {
-  class ContextMock {
-    constructor() {
-      this.action = action;
-      this.eventName = eventName;
-      this.payload = this.getPayload();
-      this.ref = "release";
-      this.sha = "6c2373ad036d292f7e9467981ba8b467e8b8b0ff";
-      this.actor = "nerrad";
-      this.workflow = "";
-    }
-    get issue() {
-      return {
-        owner: "nerrad",
-        repo: "tests",
-        number: 10,
-      };
-    }
+exports.gimmeContext = ( eventName, action ) => {
+	class ContextMock {
+		constructor() {
+			this.action = action;
+			this.eventName = eventName;
+			this.payload = this.getPayload();
+			this.ref = 'release';
+			this.sha = '6c2373ad036d292f7e9467981ba8b467e8b8b0ff';
+			this.actor = 'nerrad';
+			this.workflow = '';
+		}
+		get issue() {
+			return {
+				owner: 'nerrad',
+				repo: 'tests',
+				number: 10,
+			};
+		}
 
-    get repo() {
-      return { owner: "nerrad", repo: "tests" };
-    }
+		get repo() {
+			return { owner: 'nerrad', repo: 'tests' };
+		}
 
-    getPayload() {
-      const filename =
-        typeof action !== undefined
-          ? `${eventName}.${action}.json`
-          : `${eventName}.json`;
-      return require(`./fixtures/payloads/${filename}`);
-    }
-  }
-  return new ContextMock();
+		getPayload() {
+			const filename =
+				typeof this.action !== 'undefined'
+					? `${ eventName }.${ action }.json`
+					: `${ eventName }.json`;
+			return require( `./fixtures/payloads/${ filename }` );
+		}
+	}
+	return new ContextMock();
 };
 
 exports.gimmeOctokit = () => {
-  return {
-    issues: {
-      update: jest.fn().mockName("issues.update"),
-      createComment: jest.fn().mockName("issues.createComment"),
-      listComments: jest
-        .fn(() => Promise.resolve({ data: [] }))
-        .mockName("issues.listComments"),
-      create: jest
-        .fn((data) => Promise.resolve({ data }))
-        .mockName("issues.create"),
-    },
-    git: {
-      getCommit: jest
-        .fn(() => Promise.resolve({ data: { parents: [1] } }))
-        .mockName("git.getCommit"),
-    },
-    search: {
-      issuesAndPullRequests: jest
-        .fn(() => Promise.resolve({ data: { total_count: 0, items: [] } }))
-        .mockName("search.issuesAndPullRequests"),
-    },
-    repos: {
-      getCommit: jest.fn(() => loadDiff("basic")).mockName("repos.getCommit"),
-    },
-    pulls: {
-      get: jest.fn(() => loadDiff("basic")).mockName("pulls.get"),
-    },
-  };
+	return {
+		issues: {
+			update: jest.fn().mockName( 'issues.update' ),
+			createComment: jest.fn().mockName( 'issues.createComment' ),
+			listComments: jest
+				.fn( () => Promise.resolve( { data: [] } ) )
+				.mockName( 'issues.listComments' ),
+			create: jest
+				.fn( ( data ) => Promise.resolve( { data } ) )
+				.mockName( 'issues.create' ),
+		},
+		git: {
+			getCommit: jest
+				.fn( () => Promise.resolve( { data: { parents: [ 1 ] } } ) )
+				.mockName( 'git.getCommit' ),
+		},
+		search: {
+			issuesAndPullRequests: jest
+				.fn( () =>
+					Promise.resolve( { data: { total_count: 0, items: [] } } )
+				)
+				.mockName( 'search.issuesAndPullRequests' ),
+		},
+		repos: {
+			getCommit: jest
+				.fn( () => loadDiff( 'basic' ) )
+				.mockName( 'repos.getCommit' ),
+		},
+		pulls: {
+			get: jest.fn( () => loadDiff( 'basic' ) ).mockName( 'pulls.get' ),
+		},
+	};
 };
 
-exports.gimmeApp = (appType) => {
-  const app = require(`../lib/automations/${appType}`);
-  return app.runner;
+exports.gimmeApp = ( appType ) => {
+	const app = require( `../lib/automations/${ appType }` );
+	return app.runner;
 };
